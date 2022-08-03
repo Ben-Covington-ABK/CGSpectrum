58e8b6a6c7dc8041944dbe663cf93522988c1745
Ben-Covington-ABK <benjamin.covington@activision.com>
2022-08-03 16:06:51 -0500

    Module 03 Chapter 02 Pathfinding Solution
    I finally managed to get my A* pathfinding function working. All of this without building custom classes.

--- Modules/Module 03 Algorithms, Analysis And Multithreading/Chapter 01 - Algorithms/Algorithms/Pathfinder/Pathfinder.cpp
+++ Modules/Module 03 Algorithms, Analysis And Multithreading/Chapter 01 - Algorithms/Algorithms/Pathfinder/Pathfinder.cpp
@@ -3,53 +3,66 @@
 #include <string>
 #include <vector>
 #include <tuple>
-#include <set>
 #include <map>
 
 using namespace std;
 
-auto add(vector<tuple<char, char, int>>& g, char x, char y, int z)
+auto A_Star(map<char, vector<tuple<char, int>>> groups, char start, char goal, int h = 0)
+{
+	vector<char> open{ start };
+	vector<char> closed;
+	tuple<vector<char>, int> path{ vector<char>{start}, 0 };
+	while(!open.empty())
+	{
+		char current = open.back();
+		if(current == goal)	return path;
+		closed.push_back(current);
+		open.pop_back();
+		int offset = 999;
+		for(auto neighbor : groups[current])
+		{
+			char next = get<0>(neighbor);
+			if(!count(closed.begin(), closed.end(), next))
+			{
+				int cost = get<1>(neighbor);
+				int total = get<1>(path);
+				int newTotal = total + cost;
+				int largest = total + offset;
+				if (total == 0 || newTotal < largest)
+				{
+					offset = 0;
+					get<0>(path).push_back(next);
+					get<1>(path) = newTotal;
+					if(!count(open.begin(), open.end(), next))
+					{
+						open.push_back(next);
+					}
+				}
+			}
+		}
+	}
+	return path;
+}
+
+void add(vector<tuple<char, char, int>>& g, char x, char y, int z)
 {
 	g.push_back(tuple<char, char, int>{x, y, z});
 }
 
-
-/*
-auto reconstruct_path(map<char, int> cameFrom, char current, set<char> keys)
+auto group(vector<tuple<char, char, int>> graph)
 {
-	vector<tuple<char, int>> total_path{tuple<char, int>{current, 0}};
-	while(keys.count(current))
+	map<char, vector<tuple<char, int>>> groups;
+	for (tuple<char, char, int> t : graph)
 	{
-		current = cameFrom[current];
-		total_path.push_back(current);
+		tuple<char, int> first{ get<1>(t), get<2>(t) };
+		tuple<char, int> second{ get<0>(t), get<2>(t) };
+		groups[get<0>(t)].push_back(first);
+		groups[get<1>(t)].push_back(second);
 	}
-	return total_path;
+	return groups;
 }
-*/
 
-/*
-auto A_Star(char start, char goal, int h = 0)
-{
-	set<char> openSet { start };
-	map<char, int> cameFrom;
-	map<char, int> gScore;
-	gScore[start] = 0;
-	map<char, int> fScore;
-	fScore[start] = h;
-	while(!openSet.empty())
-	{
-		char current = start;
-		if (current == goal) 
-		{
-			return reconstruct_path(cameFrom, current, openSet);
-		}
-		openSet.erase(current);
-		
-	}
-}
-*/
-
-void printAll(vector<tuple<char, char, int>> graph)
+void printGraph(vector<tuple<char, char, int>> graph)
 {
 	for (auto t : graph) 
 	{
@@ -60,6 +73,36 @@ void printAll(vector<tuple<char, char, int>> graph)
 	}
 }
 
+void printGroups(map<char, vector<tuple<char, int>>> groups)
+{
+	for (auto& pair : groups)
+	{
+		cout << "{ " << pair.first << " : ";
+		for (int i = 0; i < pair.second.size(); i++)
+		{
+			cout << "[" << get<0>(pair.second[i])
+				<< ", " << get<1>(pair.second[i]) << "]"
+				<< (i < pair.second.size() - 1 ? ", " : " ");
+		}
+		cout << "}\n";
+	}
+}
+
+void printPath(tuple<vector<char>, int> path) 
+{
+	int length = get<0>(path).size();
+	cout << "Path from ";
+	for(int i = 0; i < length; i++)
+	{
+		cout << get<0>(path)[i];
+		if (i < length - 1)
+		{
+			cout << " to ";
+		}
+	}
+	cout << " cost " << get<1>(path);
+}
+
 int main()
 {
 	vector<tuple<char, char, int>> graph;
@@ -67,52 +110,16 @@ int main()
 	add(graph, 'b', 'c', 1);
 	add(graph, 'a', 'd', 7);
 	add(graph, 'c', 'd', 1);
+	printGraph(graph);
+	cout << endl;
+	
+	auto groups = group(graph);
+	printGroups(groups);
+	cout << endl;
 
-	map<char, vector<tuple<char, int>>> groups;
-	for(tuple<char, char, int> t : graph)
-	{
-		tuple<char, int> first{ get<1>(t), get<2>(t) };
-		tuple<char, int> second{ get<0>(t), get<2>(t) };
-		groups[get<0>(t)].push_back(first);
-		groups[get<1>(t)].push_back(second);
-	}
+	auto path = A_Star(groups, 'd', 'a');
+	printPath(path);
+	cout << endl;
 
-	for(auto &pair : groups)
-	{
-		cout << "{ " << pair.first << " : ";
-		for(tuple<char, int> t : pair.second)
-		{
-			cout << "[" << get<0>(t) << ", "
-				<< get<1>(t) << "], ";
-		}
-		cout << "}\n";
-		/*
-		cout << "{" << pair.first << ": [" 
-			<< get<0>(pair.second) << "}\n";
-		*/
-	}
-
-	//printAll(graph);
-	/*
-	int length = 10;
-	int height = 5;
-	string map = "";
-	vector<int> pos{ 1, 1 };
-	for (int y = 0; y < height; y++)
-	{
-		for (int x = 0; x < length; x++)
-		{
-			if (pos[0] == x && pos[1] == y) 
-			{
-				map += "@";
-			}
-			else
-			{
-				map += "*";
-			}
-		}
-		map += "\n";
-	}
-	cout << map << endl;	
-	*/
+	return 0;
 }
