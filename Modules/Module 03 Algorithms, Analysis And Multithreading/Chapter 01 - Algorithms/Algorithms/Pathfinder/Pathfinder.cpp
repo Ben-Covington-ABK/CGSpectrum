d6be7f9aec2a70e5f1cfc5bbf075bbe8ffa2b326
Ben-Covington-ABK <benjamin.covington@activision.com>
2022-08-04 12:04:48 -0500

    Updated Pathfinding Project
    I think I managed to get A* actually working this time. I removed the closed list as it is no longer needed. I added the gScore and fScore to track the cost from start.

--- Modules/Module 03 Algorithms, Analysis And Multithreading/Chapter 01 - Algorithms/Algorithms/Pathfinder/Pathfinder.cpp
+++ Modules/Module 03 Algorithms, Analysis And Multithreading/Chapter 01 - Algorithms/Algorithms/Pathfinder/Pathfinder.cpp
@@ -12,49 +12,128 @@ typedef vector<char> Steps;
 typedef tuple<char, char, int> Edge;
 typedef vector<Edge> Graph;
 typedef tuple<char, int> Neighbor;
+typedef map<char, Neighbor> NeighborMap;
 typedef map<char, vector<Neighbor>> NodeMap;
+typedef map<char, int> ScoreMap;
 typedef tuple<Steps, int> Path;
 
 // Define enums for tuples
-enum EDGE {first, second, cost};
+enum EDGE {first, second, length};
 enum NEIGHBOR {next, cost};
 enum PATH {steps, total};
 
+// A* finds a path from start to goal.
+// h is the heuristic defaulted to 0.
 Path A_Star(NodeMap groups, char start, char goal, int h = 0)
 {
+	// The set of discovered nodes that may need to be (re-)expanded.
+	// Initially, only the start node is know.
 	Steps open{ start };
-	Steps closed;
-	Path path{ Steps{start}, 0 };
+	
+	// Path is a set of nodes and the total cost to travel the set.
+	Path path{ open, 0 };
+
+	// For node n, cameFrom[n] is the node immediately preceding it 
+	// on the cheapest path frmo start to n currently known.
+	NeighborMap cameFrom;
+
+	// For node n, gScore[n] is the cost of the cheapest path from start.
+	ScoreMap gScore;
+
+	// For node n, fScore[n] is a guess of the cost of the cheapest path from start.
+	ScoreMap fScore;
+
+	// Default all node values to Infinity. (999 for simplicity)
+	for (auto& pairs : groups)
+	{
+		char node = pairs.first;
+		gScore[node] = node == start ? 0 : 999;
+		fScore[node] = node == start ? h : 999;
+	}
+
 	while(!open.empty())
 	{
-		char current = open.back();
-		if(current == goal)	return path;
-		closed.push_back(current);
-		open.pop_back();
-		int offset = 999;
+		int currInt = -1;		// Current Index
+		char current = NULL;	// Current Node
+
+		// Linear search to find cheapest node in open.
+		for (int i = 0; i < open.size(); i++)
+		{
+			if (current == NULL || fScore[open[i]] < fScore[current])
+			{
+				currInt = i;
+				current = open[i];
+			}
+		}
+
+		// When we find the goal fill and return the path.
+		if (current == goal)
+		{
+			int total = get<NEIGHBOR::cost>(cameFrom[goal]);
+			path = Path{ Steps{ goal }, total };
+			while (cameFrom.count(current))
+			{
+				Neighbor neighbor = cameFrom[current];
+				char next = get<NEIGHBOR::next>(neighbor);
+				int cost = get<NEIGHBOR::cost>(neighbor);
+				auto begin = get<PATH::steps>(path).begin();
+				get<PATH::steps>(path).insert(begin, next);
+				current = next;
+			}
+			return path;
+		}
+
+		// Remove the node at current index from open.
+		open.erase(open.begin() + currInt);
+
+		// For each neighbor of the current node. 
 		for(Neighbor neighbor : groups[current])
 		{
-			char next = get<NEIGHBOR::next>(neighbor);
-			if(!count(closed.begin(), closed.end(), next))
+			char next = get<NEIGHBOR::next>(neighbor);	// Neighbor Node 
+			int cost = get<NEIGHBOR::cost>(neighbor);	// Neighbor Length
+
+			// tentative is the distance from start to the neighbor through current.
+			int tentative = gScore[current] + cost;
+			if (tentative < gScore[next])
 			{
-				int cost = get<NEIGHBOR::cost>(neighbor);
-				int total = get<PATH::total>(path);
-				int newTotal = total + cost;
-				int largest = total + offset;
-				if (total == 0 || newTotal < largest)
+				// Record the path to neighbor as it is better than any previous one.
+				cameFrom[next] = Neighbor{ current, tentative };
+				gScore[next] = tentative;
+				fScore[next] = tentative + h;
+
+				// Add neighbor if they are not in open.
+				if(!count(open.begin(), open.end(), next))
 				{
-					offset = 0;
-					get<PATH::steps>(path).push_back(next);
-					get<PATH::total>(path) = newTotal;
-					if(!count(open.begin(), open.end(), next))
-					{
-						open.push_back(next);
-					}
+					open.push_back(next);
 				}
 			}
 		}
 	}
+	// Open set is empty but goal was never reached.
 	return path;
+}
+
+void addNeighbor(NodeMap& groups, char node, Neighbor neighbor)
+{
+	if (groups[node].size() == 0) groups[node].push_back(neighbor);
+	else
+	{
+		for (int i = 0; i < groups[node].size(); i++)
+		{
+			char next = get<NEIGHBOR::next>(groups[node][i]);
+			int cost = get<NEIGHBOR::cost>(groups[node][i]);
+			if (get<NEIGHBOR::cost>(neighbor) < cost) 
+			{
+				groups[node].insert(groups[node].begin() + i, neighbor);
+				break;
+			}
+			else if (i >= groups[node].size() - 1)
+			{
+				groups[node].push_back(neighbor);
+				break;
+			}
+		}
+	}
 }
 
 NodeMap group(Graph graph)
@@ -62,10 +141,11 @@ NodeMap group(Graph graph)
 	NodeMap groups;
 	for (Edge edge : graph)
 	{
-		Neighbor first{ get<EDGE::second>(edge), get<EDGE::cost>(edge) };
-		Neighbor second{ get<EDGE::first>(edge), get<EDGE::cost>(edge) };
-		groups[get<EDGE::first>(edge)].push_back(first);
-		groups[get<EDGE::second>(edge)].push_back(second);
+		char x = get<EDGE::first>(edge);
+		char y = get<EDGE::second>(edge);
+		int len = get<EDGE::length>(edge);
+		addNeighbor(groups, x, Neighbor{ y, len });
+		addNeighbor(groups, y, Neighbor{ x, len });
 	}
 	return groups;
 }
@@ -77,7 +157,7 @@ void printGraph(Graph graph)
 		cout << "(" 
 			<< get<EDGE::first>(edge) << ", "
 			<< get<EDGE::second>(edge) << ", " 
-			<< get<EDGE::cost>(edge) << ")\n";
+			<< get<EDGE::length>(edge) << ")\n";
 	}
 }
 
@@ -118,6 +198,8 @@ int main()
 	graph.push_back(Edge{ 'b', 'c', 1 });
 	graph.push_back(Edge{ 'a', 'd', 7 });
 	graph.push_back(Edge{ 'c', 'd', 1 });
+	graph.push_back(Edge{ 'd', 'e', 1 });
+	graph.push_back(Edge{ 'b', 'e', 1 });
 
 	printGraph(graph);
 	cout << endl;
@@ -126,7 +208,7 @@ int main()
 	printGroups(groups);
 	cout << endl;
 
-	Path path = A_Star(groups, 'a', 'd');
+	Path path = A_Star(groups, 'a', 'e');
 	printPath(path);
 	cout << endl;
 
